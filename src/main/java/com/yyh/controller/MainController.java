894376822f71c1c4617ad48439db8f2b60efe590
yyh1995 <yyh1995116@gmail.com>
2017-03-29 19:52:38 +0800

    yyh

--- src/main/java/com/yyh/controller/MainController.java
+++ src/main/java/com/yyh/controller/MainController.java
@@ -4,6 +4,7 @@ import com.yyh.pojo.Funds;
 import com.yyh.pojo.FundsExample;
 import com.yyh.service.FundsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,17 +16,21 @@ import java.util.List;
  */
 @Controller
 public class MainController {
+    Funds funds;
     FundsService fundsService;
     FundsExample fundsExample;
 
     //return "index"：处理完该请求后返回的页面，此请求返回 名为index.jsp页面
     @RequestMapping(value="/",method= RequestMethod.GET)
     public String index(){
-
-        List<Funds> funds=fundsService.getFunds(fundsExample);
         return "index";
     }
 
+    @RequestMapping(value = "/")
+    public String getAllFunds(){
+        List<Funds> funds=fundsService.getFunds(fundsExample);
+        return "index";
+    }
 
     @RequestMapping("/hello" )
     public String hello(){
